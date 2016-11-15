2e94a687692b5e999e5ebb07e83ac22d80ae7a5c
Joe Ferner <joe@fernsroth.com>
2016-11-14 22:24:44 -0500

    fix compiling

--- index.ts
+++ index.ts
@@ -3,12 +3,12 @@ import {Device, DeviceOptions} from "unisonht/lib/Device";
 interface YamahaReceiverOptions extends DeviceOptions {
   address: string;
   inputs: {
-    [deviceInputName: string];
+    [deviceInputName: string]: string;
   }
 }
 
 export default class YamahaReceiver extends Device {
-  constructor(options: YamahaReceiverOptionsOptions) {
+  constructor(options: YamahaReceiverOptions) {
     super(options);
   }
 }
